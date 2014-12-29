fcd2de70c9acea1e14afb30e68295be38c50e287
Sari Dan <sari@brillix.co.il>
2014-12-29 19:34:11 +0200

    Move comma position to the end

--- QueryIndividuals+Dates+ShortNames.sql
+++ QueryIndividuals+Dates+ShortNames.sql
@@ -1,4 +1,4 @@
-select 	gti.GenTreeId AS GT, 
+	select 	gti.GenTreeId AS GT, 
 		gti.IndividualId AS II, 
 		gti.BirthDate As BD, 
 		gti.BirthPlace AS BP, 
@@ -25,10 +25,10 @@ select 	gti.GenTreeId AS GT,
 		Birth.PeriodEndDate AS BED,
 		/*Death Date*/
 		Death.PeriodStartDate AS DSD,
-		Death.PeriodEndDate AS DED--,
+		Death.PeriodEndDate AS DED,
 		/*Marriage Date*/
-		SUBSTRING (STUFF(( SELECT ',' + cast(Marriage.PeriodStartDate as varchar(100)) FROM dbo.GenTreePeriod Marriage where gti.GenTreeId=Marriage.GenTreeId and gti.IndividualId=Marriage.IndividualId and Marriage.PeriodTypeCode= 7 order by Marriage.PeriodStartDate for XML PATH(''),Type).value('.','NVARCHAR(MAX)'),1,0,''),2) AS MSD,
-		SUBSTRING (STUFF(( SELECT ',' + cast(Marriage.PeriodEndDate as varchar(100)) FROM dbo.GenTreePeriod Marriage where gti.GenTreeId=Marriage.GenTreeId and gti.IndividualId=Marriage.IndividualId and Marriage.PeriodTypeCode= 7 order by Marriage.PeriodStartDate for XML PATH(''),Type).value('.','NVARCHAR(MAX)'),1,0,'') AS MED
+		STUFF(( SELECT  cast(Marriage.PeriodStartDate as varchar(100)) + ',' FROM dbo.GenTreePeriod Marriage where gti.GenTreeId=Marriage.GenTreeId and gti.IndividualId=Marriage.IndividualId and Marriage.PeriodTypeCode= 7 order by Marriage.PeriodStartDate for XML PATH(''),Type).value('.','NVARCHAR(MAX)'),1,0,'') AS MSD,
+		STUFF(( SELECT  cast(Marriage.PeriodEndDate as varchar(100)) + ',' FROM dbo.GenTreePeriod Marriage where gti.GenTreeId=Marriage.GenTreeId and gti.IndividualId=Marriage.IndividualId and Marriage.PeriodTypeCode= 7 order by Marriage.PeriodStartDate for XML PATH(''),Type).value('.','NVARCHAR(MAX)'),1,0,'') AS MED
 from GenTreeIndividuals gti
 full join [dbo].[GenTree] gt on gt.GenTreeId=gti.GenTreeId
 left join  
