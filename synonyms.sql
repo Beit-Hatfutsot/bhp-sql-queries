f3452cd906548f5abe59a7db7ef66019e11a1b80
Pavel Suchman <pavel.suchman@gmail.com>
2015-03-05 14:23:44 +0200

    Using plain select *

--- synonyms.sql
+++ synonyms.sql
@@ -1,5 +1,5 @@
-select  syno1.Num,
-STUFF(( SELECT  cast(syno2.Synonym as nvarchar(max))+ ','FROM synonyms syno2 where syno1.Num = syno2.Num  order by syno2.Synonym for XML PATH(''),Type).value('.','NVARCHAR(MAX)'),1,0,'') SynonymText,
-STUFF(( SELECT  cast(syno2.LanguageCode as nvarchar(max))+ ','FROM synonyms syno2 where syno1.Num = syno2.Num  order by syno2.Synonym for XML PATH(''),Type).value('.','NVARCHAR(MAX)'),1,0,'') SynoLangN
-from synonyms syno1
-group by syno1.Num;
+SELECT [SynonymKey]
+      ,[Num]
+      ,[LanguageCode]
+      ,[Synonym]
+  FROM [BHP_FINAL].[dbo].[Synonyms]
