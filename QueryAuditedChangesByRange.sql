d978018457f7e3a4e9be3ef09f7b9204f1494074
Sari Dan <sari@brillix.co.il>
2015-05-06 15:37:22 +0300

    Adding distinction

--- QueryAuditedChangesByRange.sql
+++ QueryAuditedChangesByRange.sql
@@ -1,6 +1,6 @@
 -- INSERT/UPDATE
-select	operationslog.operationDate, 
-		operationslog.UnitId
+select max(operationslog.operationDate), 
+	   operationslog.UnitId
 from operationslog
 left join Units 
 on operationslog.UnitId=Units.UnitId and operationslog.IsSynonym=0
@@ -12,6 +12,7 @@ and (case OperationDescription when 'CHANGE_STATUS' then 'UPDATE' else Operation
 and operationDate >= dateadd(s, from_date, '19700101')
 and operationDate <= dateadd(s, to_date, '19700101')
 and isnull(Units.UnitType, 0)=unit_type
+group by operationslog.UnitId
 
 -- DELETE
 select	operationslog.operationDate, 
