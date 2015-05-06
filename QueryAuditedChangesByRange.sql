6a432b396dd1bbf21b044aac351eb13ea115d0a3
Sari Dan <sari@brillix.co.il>
2015-05-06 17:54:16 +0300

    Adding distinction

--- QueryAuditedChangesByRange.sql
+++ QueryAuditedChangesByRange.sql
@@ -14,12 +14,11 @@ and operationDate <= dateadd(s, to_date, '19700101')
 and isnull(Units.UnitType, 0)=unit_type
 group by operationslog.UnitId
 
--- DELETE
+-- DELETE (Synonyms only)
 select	operationslog.operationDate, 
-		operationslog.UnitId,
-		isnull(Units.UnitType, 15)
+		operationslog.UnitId
 from operationslog
-left join Units on operationslog.UnitId=Units.UnitId and operationslog.IsSynonym=0
+--left join Units on operationslog.UnitId=Units.UnitId and operationslog.IsSynonym=0
 where OperationDescription='DELETE'
-and operationDate >= from_date
-and operationDate <= to_date;
+and operationDate >= dateadd(s, from_date, '19700101')
+and operationDate <= dateadd(s, to_date, '19700101');
