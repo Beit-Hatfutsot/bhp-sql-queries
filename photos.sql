5b63a4b9a03a7372a6807362a82f51d51b726f54
Danny Berger <dannyb@bh.org.il>
2015-05-27 18:40:12 +0300

    add parameter support to photos query

--- photos.sql
+++ photos.sql
@@ -1,1 +1,3 @@
-SELECT cast(PictureId as varchar(max)) as PictureId, PicturePath, PictureFileName FROM Pictures with (nolock)
+SELECT cast(PictureId as varchar(max)) as PictureId, PicturePath, PictureFileName 
+FROM Pictures with (nolock)
+WHERE PictureId=PictureId AND (%s=0 OR PictureId IN (%s))
